c35509be28a4ba8ff267fef2a3503f8db5dbbd58
umahmood <umahmood@users.noreply.github.com>
2017-09-09 22:53:47 +0100

    Fix version typo in setup file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name = 'simple-telegrambot',
   packages = ['simple-telegrambot'], 
-  version = '1.0.1',
+  version = '1.0.0',
   description = 'A simple Telegram bot which sends and receive messages',
   author = 'Usman Mahmood',
   author_email = 'um@blackhole.com',
